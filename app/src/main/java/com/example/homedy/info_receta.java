7ed323fcf5e3bffd1021054b2c9c8e9696ba8865
Cristian Isaac Nolasco Martinez <usis033721@ugb.edu.sv>
2023-05-07 10:51:32 -0600

    ¨completo y no  terminado

---
+++ app/src/main/java/com/example/homedy/info_receta.java
@@ -0,0 +1,15 @@
+package com.example.homedy;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class info_receta extends AppCompatActivity {
+
+    @SuppressLint("MissingInflatedId")
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dolorestomago1);
+
+    }     }
